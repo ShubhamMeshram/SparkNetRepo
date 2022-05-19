520c4facf586fe75ed9e03154ad19d8aa5efb94c
smeshram <smeshram@bjs.com>
2022-05-19 12:05:12 +0530

    code changes..

---
+++ code/sample.py
@@ -0,0 +1,14 @@
+from multiprocessing import JoinableQueue
+
+import yaml
+from common.SetupSpark import JobManager
+
+
+def ReadConfigFile(yaml_file):
+    with open("conf/spark_net.yaml") as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    return config
+
+
+job = JobManager("dna_mbr_mfi", config_path="conf/spark_net.yaml")
+print(job)
