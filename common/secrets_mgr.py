e1373cfb691297dae05cf734799fd5d42b3d22ed
smeshram <smeshram@bjs.com>
2022-05-19 16:38:22 +0530

    changes to secrets-mgr code

--- common/secrets_mgr.py
+++ common/secrets_mgr.py
@@ -59,13 +59,12 @@ def get_secret():
         # Decrypts secret using the associated KMS key.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
         if "SecretString" in get_secret_value_response:
-            secret = base64.b64decode(
-                get_secret_value_response["SecretString"]
-            )
+            secret = get_secret_value_response["SecretString"]
             actual_secret = ast.literal_eval(secret).get("secret-key")
-            return actual_secret
+            return str.encode(actual_secret)
         else:
             print("inside inner else")
             decoded_binary_secret = base64.b64decode(
                 get_secret_value_response["SecretBinary"]
             )
+            return decoded_binary_secret
