e1da48e654bb32abdfb1fe12982e59152a6a2153
smeshram <smeshrammesh1@gmail.com>
2022-05-19 22:53:53 +0530

    test code

--- code/sec-test.py
+++ code/sec-test.py
@@ -1,7 +1,12 @@
 from common.secrets_mgr import get_secret
+
 print(get_secret("sparknet-crpyt-key"))
-print(get_secret("admin-ak"))
-print(get_secret("admin-sak"))
-print(type(get_secret("sparknet-crpyt-key")))
-print(type(get_secret("admin-ak")))
-print(type(get_secret("admin-sak")))
+# print(get_secret("admin-ak"))
+# print(get_secret("admin-sak"))
+# print(type(get_secret("sparknet-crpyt-key")))
+# print(type(get_secret("admin-ak")))
+# print(type(get_secret("admin-sak")))
+
+
+coll = ("firstName", "lastName", "address", "email")
+print(type(coll))
