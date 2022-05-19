4dab157bba3ea72789690d066481d9df31aeb1f1
smeshram <smeshrammesh1@gmail.com>
2022-05-19 21:07:55 +0530

    dummy debug prints

--- common/secrets_mgr.py
+++ common/secrets_mgr.py
@@ -58,6 +58,7 @@ def get_secret(secret_name):
         # Decrypts secret using the associated KMS key.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
         if "SecretString" in get_secret_value_response:
+            print(get_secret_value_response)
             secret = get_secret_value_response["SecretString"]
             print(secret)
             print(type(secret))
