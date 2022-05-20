f095f15000d397c3839430f12678873964c31126
smeshram <smeshram@bjs.com>
2022-05-20 11:26:16 +0530

    changes to crpyt functions

--- code/driver.py
+++ code/driver.py
@@ -94,21 +94,17 @@ def encryption_fn(usr_df, col_tuple):
 
 
 def decryption_fn(usr_df, col_tuple):
-    # usr_df = usr_df.withColumn(
-    #    "fname_de", decrypt_message_udf(col("fname_en"))
-    # )
-    # return usr_df
     for name in usr_df.schema.names:
         usr_df = usr_df.withColumnRenamed(name, name.replace("_en", ""))
     col_tuple = [
         tuple(map(lambda i: str.replace(i, "_en", ""), tup))
         for tup in col_tuple
     ]
+    print(col_tuple)
     print(usr_df.printSchema())
     for column in col_tuple:
-        usr_df = usr_df.withColumn(
-            column , decrypt_message_udf(col(column))
-        )
+        print(column)
+        usr_df = usr_df.withColumn(column, decrypt_message_udf(col(column)))
     temp_df = usr_df.drop(*col_tuple)
     return temp_df
 
